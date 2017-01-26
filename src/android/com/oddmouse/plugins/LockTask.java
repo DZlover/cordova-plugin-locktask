cbac58dc9706c7aaacd5722d7aeaaad049a52add
DZlover <cherifmetarfi@gmail.com>
2017-01-26 11:19:46 +0100

    Update LockTask.java

--- src/android/com/oddmouse/plugins/LockTask.java
+++ src/android/com/oddmouse/plugins/LockTask.java
@@ -46,7 +46,7 @@ public class LockTask extends CordovaPlugin {
 
           }
 
-          activity.startLockTask();
+          activity.setLockTaskPackages();
         }
 
         callbackContext.success();
